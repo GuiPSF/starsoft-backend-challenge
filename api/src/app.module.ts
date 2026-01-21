47d23f7849478ef7831c7e5638c6573397bd61e2
Guilherme Ferreira <guilherme-psferreira@outlook.com>
2026-01-21 00:07:03 -0300

    chore: create session files and seat module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SessionModule } from './session/session.module';
+import { SeatModule } from './seat/seat.module';
 
 @Module({
-  imports: [],
+  imports: [SessionModule, SeatModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+TypeOrmModule.forRoot({
+  type: "postgres",
+  host: "postgres",
+  port: 5432,
+  username: "cinema",
+  password: "cinema",
+  database: "cinema",
+  autoLoadEntities: true,
+  synchronize: true,
+});
